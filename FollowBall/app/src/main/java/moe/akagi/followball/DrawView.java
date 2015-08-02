f347060b598fe7848cecbf7ee53506296168c715
Yunze <mayunzhe1995@gmail.com>
2015-08-02 23:07:49 +0800

    First commit

---
+++ FollowBall/app/src/main/java/moe/akagi/followball/DrawView.java
@@ -0,0 +1,40 @@
+package moe.akagi.followball;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by myz on 2015/8/2.
+ */
+public class DrawView extends View {
+    public float currentX = 40;
+    public float currentY = 50;
+
+    Paint p = new Paint();
+    public DrawView(Context context) {
+        super(context);
+    }
+    public DrawView(Context context, AttributeSet set) {
+        super(context, set);
+    }
+    @Override
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        p.setColor(Color.RED);
+        canvas.drawCircle(currentX, currentY, 15, p);
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        currentX = event.getX();
+        currentY = event.getY();
+
+        invalidate();
+
+        return true;
+    }
+}
